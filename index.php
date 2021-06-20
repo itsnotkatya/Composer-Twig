8747306d21b21e03d72ae76759ca3afd61c869c7
itsnotkatya <49903775+itsnotkatya@users.noreply.github.com>
2021-06-20 20:13:35 +1000

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'vendor\autoload.php'; //подключаем файл автозагрузки композера
+
+Twig_Autholoader::register(); //авторизируем и регистрируем шаблонизатор
+
+$loader = new Twig_Loader_Filesistem('templates'); //указываем папку для хранения шаблонов
+$twig=new Twig_Environment($loader, array('cache' => 'cache'));
+$template=$twig->loadTemplate('index.twig'); //подгружаем файл с шаблоном
+$title ="Composer+Twig";
+
+$flower=array(
+  array('title'=>'Rose','price'=>'3$'),
+  array('title'=>'Hibiskus','price'=>'2$'),
+  array('title'=>'Lavender','price'=>'1$'),
+  array('title'=>'Tulip','price'=>'5$'),
+  array('title'=>'Pion','price'=>'4$'),
+);
+echo $template->render('nav.twig',array('flower'=>$flower));
+
+?>
